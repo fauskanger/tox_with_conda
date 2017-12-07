219c87fd5eb62128085d139437ea709c45fd4396
Thomas Fauskanger <thomasfauskanger@gmail.com>
2017-12-07 04:19:50 +0100

    add setup.py, uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='tox_with_conda',
+    version='1.0.1',
+    packages=[''],
+    py_modules=['tox_with_conda'],
+    url='https://github.com/fauskanger/tox_with_conda',
+    license='MIT',
+    author='Thomas Fauskanger',
+    author_email='',
+    description='Utility for making conda environments work with tox',
+    long_description='See example of use at https://github.com/fauskanger/tox_with_conda'
+)
